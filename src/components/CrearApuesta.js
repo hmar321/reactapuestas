b260a87b7b31fe2076117955df991e7fa46e798a
hmar321 <hmauricio.ar@gmail.com>
2023-10-27 13:57:47 +0200

    27/10/2023

--- src/components/CrearApuesta.js
+++ src/components/CrearApuesta.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Global from "../Global";
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 export default class CrearApuesta extends Component {
+    state = {
+        statusInsert: false
+    }
+
     cajaUsuario = React.createRef();
     cajaResultado = React.createRef();
     cajaFecha = React.createRef();
 
-    crearApuesta=()=>{}
+    insertarApuesta = (e) => {
+        e.preventDefault();
+        var usuario = this.cajaUsuario.current.value;
+        var resultado = this.cajaResultado.current.value;
+        var fecha = this.cajaFecha.current.value;
+        var apuesta = {
+            idApuesta: 0,
+            usuario: usuario,
+            resultado: resultado,
+            fecha: fecha
+        }
+        var request = "api/apuestas";
+        var url = Global.urlApiApuestas + request;
+        console.log(url);
+        axios.post(url, apuesta).then(res => {
+            this.setState({
+                statusInsert: true
+            });
+        });
+    }
     render() {
         return (
             <div className='container'>
+                {
+                    this.state.statusInsert===true&&
+                    (<Navigate to="/apuestas"/>)
+                }
                 <h1>Crear Apuesta</h1>
                 <form>
                     <div className='mb-3'>
@@ -23,7 +53,7 @@ export default class CrearApuesta extends Component {
                         <label className='form-label'>Fecha</label>
                         <input className='form-control' ref={this.cajaFecha} type='text' placeholder='Fecha' />
                     </div>
-                    <button className='btn btn-success'>Crear</button>
+                    <button onClick={this.insertarApuesta} className='btn btn-success'>Crear</button>
                 </form>
             </div>
         )
