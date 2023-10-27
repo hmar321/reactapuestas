56c4efb9fb2e02c37f7d687a777c0cfcebeadb86
hmar321 <hmauricio.ar@gmail.com>
2023-10-27 12:48:52 +0200

    asd

--- src/components/CrearApuesta.js
+++ src/components/CrearApuesta.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
 
 export default class CrearApuesta extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <h1>Crear Apuesta</h1>
-        <form>
-            <div className='mb-3'>
-                <label className='form-label'>Texto</label>
-                <input className='form-control' ref={this.cajaUsuario} type='text' placeholder='texto'/>
+    cajaUsuario = React.createRef();
+    cajaResultado = React.createRef();
+    cajaFecha = React.createRef();
+
+    crearApuesta=()=>{}
+    render() {
+        return (
+            <div className='container'>
+                <h1>Crear Apuesta</h1>
+                <form>
+                    <div className='mb-3'>
+                        <label className='form-label'>Usuario</label>
+                        <input className='form-control' ref={this.cajaUsuario} type='text' placeholder='Usuario' />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>Resultado</label>
+                        <input className='form-control' ref={this.cajaResultado} type='text' placeholder='Resultado' />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>Fecha</label>
+                        <input className='form-control' ref={this.cajaFecha} type='text' placeholder='Fecha' />
+                    </div>
+                    <button className='btn btn-success'>Crear</button>
+                </form>
             </div>
-            <div className='mb-3'>
-                <label className='form-label'>Texto</label>
-                <input className='form-control' ref={this.cajaResultado} type='text' placeholder='texto'/>
-            </div>
-            <div className='mb-3'>
-                <label className='form-label'>Texto</label>
-                <input className='form-control' ref={this.cajaFecha} type='text' placeholder='texto'/>
-            </div>
-            <button className='btn btn-success'>Crear</button>
-        </form>
-      </div>
-    )
-  }
+        )
+    }
 }
